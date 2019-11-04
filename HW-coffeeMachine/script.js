72bdd518ae5f222c3c62c55ebc3f0e72101a4da3
maryapple <maria9ku@gmail.com>
2019-11-04 22:12:15 +0300

    Problem with disabling the buttons

--- HW-coffeeMachine/script.js
+++ HW-coffeeMachine/script.js
@@ -1,4 +1,4 @@
-class StandardDrink {
+/* class StandardDrink {
     constructor(name, price, volume) {
         this.name = name;
         this.price = price;
@@ -6,18 +6,60 @@ class StandardDrink {
     }
 }
 
+class AuthorDrink extends StandardDrink {
+
+}
+
 const espresso = new StandardDrink('Эспрессо', 90, 100);
 const latte = new StandardDrink('Латте', 130, 250);
 const cappuccino = new StandardDrink('Каппучино', 110, 250);
 const bananaLatte = new StandardDrink('Банановый латте', 150, 300);
 const vanillaCappuccino = new StandardDrink('Ванильный каппучино', 150, 300);
-const flatWhite = new StandardDrink('Флэт уайт', 100, 280);
+const flatWhite = new StandardDrink('Флэт уайт', 100, 280); */
+
+/* const machine = {
+    init() {
+        
+        const pressedBtn = document.getElementsByClassName('item')
+
+        alert('pressed: ' + pressedBtn)
+    }
+}
+init() */
+
+const syrupBtn = document.querySelector('#cherrySyrup')
+const paymentBtn = document.querySelector('#payment')
+
+
+const coffee = [...document.getElementsByClassName('btn')];
+console.log(coffee)
+
+
+syrupBtn.classList.add('disabled')
+paymentBtn.classList.add('disabled')
+coffee.forEach(function(elem) {
+    
+    elem.addEventListener('click', function() {
+        const btnName = this.innerText
+        const chosenBtn = document.getElementsByClassName('item')
+        if (chosenBtn !== undefined) {
+            console.log(this.innerText)
+            syrupBtn.classList.add('btn')
+            paymentBtn.classList.add('btn')
+        }
+
+        /* else if (chosenBtn === undefined){
+            syrupBtn.classList.add('disabled')
+            paymentBtn.classList.add('diabled')
+        } */
+    })
+})
+
+
 
 const price = document.querySelector('#price');
 
-const coffee = 
-
-const espressoBtn = document.querySelector('#espresso');
+/* const espressoBtn = document.querySelector('#espresso');
 espressoBtn.addEventListener('click', () => {
     currentOrder(espresso);
 })
@@ -45,10 +87,7 @@ vanillaCappuccinoBtn.addEventListener('click', () => {
 const flatWhiteBtn = document.querySelector('#flatWhite');
 flatWhiteBtn.addEventListener('click', () => {
     currentOrder(flatWhite);
-})
-
-// const chosenCoffeeBtn = document.querySelector('.menu .authorsDrink');
-
+}) */
 
 function currentOrder (nameOfDrink) {
     price.textContent = `Стоимость ${nameOfDrink.name}: ` + nameOfDrink.price + 'рублей';
