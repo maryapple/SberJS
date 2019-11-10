c84a7d34c292af5f3460bdc120adc90528bcd1f7
maryapple <maria9ku@gmail.com>
2019-11-10 23:06:59 +0300

    The volume of drink is tracked

--- HW-coffeeMachine/script.js
+++ HW-coffeeMachine/script.js
@@ -1,86 +1,100 @@
 // Массив из кнопок, которые можно нажимать в начальном состоянии, когда нельзя жать оплату и сироп
 const coffeeBtns = [...document.getElementsByClassName('coffee')];
 
-// Дисплэй с информацией по выбранному напитку
+// Дисплей с информацией по выбранному напитку
 const drinkName = document.querySelector('#drinkName')
 const priceValue = document.querySelector('#priceValue')
+const milkDisplay = document.querySelector('#milkDisplay')
+const cherrySyrupDisplay = document.querySelector('#cherrySyrupDisplay')
 
+// Кнопки добавления топпинга и оплаты
 const payBtn = document.querySelector('#payBtn')
 const cherrySyrupBtn = document.querySelector('#cherrySyrupBtn')
 const milkBtn = document.querySelector('#milkBtn')
-
-const milkDisplay = document.querySelector('#milkDisplay')
-const cherrySyrupDisplay = document.querySelector('#cherrySyrupDisplay')
 
 const menu = [
     {
         name: "Эспрессо",
         price: 90,
-        size: 100,
+        volume: 100,
         type: 'standard'
     },
     {
         name: "Латте",
         price: 130,
-        size: 250,
+        volume: 250,
         type: 'standard'
     },
     {
         name: "Каппучино",
         price: 110,
-        size: 250,
+        volume: 250,
         type: 'standard'
     },
     {
         name: "Банановый латте",
         price: 150,
-        size: 300,
+        volume: 300,
         type: 'authors'
     },
     {
         name: "Ванильный каппучино",
         price: 150,
-        size: 300,
+        volume: 300,
         type: 'authors'
     },
     {
         name: "Флэт уайт",
         price: 100,
-        size: 280,
+        volume: 280,
         type: 'authors'
     },
     {
         name: "Молоко",
         price: 25,
-        size: 50,
+        volume: 50,
         type: 'standard'
     },
     {
         name: "Вишневый сироп",
         price: 35,
-        size: 50,
+        volume: 50,
         type: 'syrup'
     }
 ]
-
-let currentPrice = 0
-let currentType = ''
 
 let topping = {
     syrup: 0,
     milk: 0
 }
 
+const mediumCup = {
+    volume: 250,
+    amount: 5
+}
+
+const largeCup = {
+    volume: 380,
+    amount: 6
+}
+
+// Параметры текущего выбранного напитка
+let currentPrice = 0
+let currentType = ''
+let currentVolume = 0
+
 coffeeBtns.forEach( (elem) => {
     elem.addEventListener('click', () => {
         // Если выбран кофе
         if ((elem.innerText !== 'Молоко') && (elem.innerText !== 'Вишневый сироп')) {
-            // Записываем название и цену выбранного напитка на экран
+            // Сбрасываем параметры для отображения топпингов и выбор топпингов
             topping.milk = 0
             topping.syrup = 0
-            drinkName.innerHTML = elem.innerText
             milkDisplay.innerText = ''
             cherrySyrupDisplay.innerText = ''
+
+            // Записываем название и цену выбранного напитка на экран
+            drinkName.innerHTML = elem.innerText
             let currentObject = menu.filter((obj) => {
                 if (obj.name === elem.innerText) {
                     return obj.price
@@ -90,6 +104,11 @@ coffeeBtns.forEach( (elem) => {
             currentType = currentObject.type
             currentPrice = currentObject.price
             priceValue.innerHTML = currentPrice
+
+            // Запоминаем объем выбранного напитка
+            currentVolume = currentObject.volume
+            console.log(currentVolume)
+            
             // В зависимости от типа напитка активируем/деактивируем кнопку выбора сиропа и молока
             if (currentType === 'standard') {
                 activateBtnSyrup()
@@ -103,13 +122,15 @@ coffeeBtns.forEach( (elem) => {
             }
         }
 
-        // Если выбрано молоко как отдельный напиток
+        // Если молоко выбрано как отдельный напиток
         else if ((elem.innerText === 'Молоко') && (drinkName.innerText === '')) {
-            // Записываем название и цену выбранного напитка на экран
+            // Сбрасываем параметры для отображения топпингов и выбор топпингов
             topping.milk = 0
             topping.syrup = 0
             milkDisplay.innerText = ''
             cherrySyrupDisplay.innerText = ''
+
+            // Записываем название и цену выбранного напитка на экран
             drinkName.innerHTML = elem.innerText
             let currentObject = menu.filter((obj) => {
                 if (obj.name === elem.innerText) {
@@ -119,39 +140,79 @@ coffeeBtns.forEach( (elem) => {
             currentObject = currentObject[0]
             currentPrice = currentObject.price
             priceValue.innerHTML = currentPrice
+
+            // Запоминаем объем напитка
+            currentVolume = currentObject.volume
+            console.log(currentVolume)
+            
+            // Активируем кнопки топпингов
             activateBtnSyrup()
             activateBtnPay()
         }
 
         // Если молоко выбрано как добавка к кофе
-        else if ((elem.innerText === 'Молоко') && (drinkName.innerText !== '') && (currentType !== 'authors')) {
+        else if (
+            (elem.innerText === 'Молоко') && 
+            (drinkName.innerText !== '') && 
+            (currentType !== 'authors') &&
+            (currentVolume <= (largeCup.volume - 50))
+        ) {
+            // Увеличиваем счетчик топпинга
             topping.milk ++
+
+            // Отображаем на экране добавку
             milkDisplay.innerText = 'с молоком' + ` x[${topping.milk}]`
+
+            // Выводим увеличенную цену напитка
             currentPrice += 25
             priceValue.innerHTML = currentPrice
+
+            // Запоминаем объем
+            currentVolume += 50
+            console.log(currentVolume)
+
+            // Смотрим, будет ли при следующем добавлении молока или 
+            // сиропа превышение объема стакана. Если да, то тогда деактивируем кнопки топпингов
+            if (currentVolume + 50 > largeCup.volume) {
+                disableBtnMilk()
+                disableBtnSyrup()
+            }
         }
 
         // Выбран вишневый сироп
         else if (
-                (elem.innerText === 'Вишневый сироп') && 
-                (currentType !== 'authors') && 
-                (cherrySyrupDisplay.innerText !== '+ двойная порция вишневого сиропа') &&
-                ( !cherrySyrupBtn.classList.contains('disabled'))
-            ) {
-                console.log(elem.innerText)
-                if (cherrySyrupDisplay.innerText === '') {
-                    cherrySyrupDisplay.innerText = '+ порция вишневого сиропа'
-                    currentPrice += 35
-                    priceValue.innerHTML = currentPrice
-                    topping.syrup ++
-                }
-                else if (cherrySyrupDisplay.innerText === '+ порция вишневого сиропа') {
-                    cherrySyrupDisplay.innerText = '+ двойная порция вишневого сиропа'
-                    currentPrice += 35
-                    priceValue.innerHTML = currentPrice
-                    disableBtnSyrup()
-                    topping.syrup ++
-                }
+            (elem.innerText === 'Вишневый сироп') && 
+            (currentType !== 'authors') && 
+            (topping.syrup < 2) &&
+            ( !cherrySyrupBtn.classList.contains('disabled')) &&
+            (currentVolume <= (largeCup.volume - 50))
+        ) {
+            // Увеличиваем счетчик топпинга
+            topping.syrup ++
+
+            // Выводим сообщение о порции сиропа
+            if (topping.syrup === 1) {
+                cherrySyrupDisplay.innerText = '+ порция вишневого сиропа'
+            }
+            else if (topping.syrup === 2) {
+                cherrySyrupDisplay.innerText = '+ двойная порция вишневого сиропа'
+                disableBtnSyrup()
+            }
+
+            // Выводим увеличенную цену
+            currentPrice += 35
+            priceValue.innerHTML = currentPrice
+
+            // Запоминаем объем напитка
+            currentVolume += 50
+            console.log(currentVolume)
+
+            // Аналогично молоку, смотрим, будет ли при следующем добавлении молока или 
+            // сиропа превышение объема стакана. Если да, то тогда деактивируем кнопки топпингов
+            if (currentVolume + 50 > largeCup.volume) {
+                disableBtnMilk()
+                disableBtnSyrup()
+            }
         }
     })
 })
